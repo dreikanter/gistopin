00369dea5ba3900b5b418dec24455d13d524f5ea
Alex Musayev <alex.musayev@gmail.com>
2012-05-31 20:56:05 +0400

    Configuration fixed. get_new_gists() simplified.

--- gistopin.py
+++ gistopin.py
@@ -3,16 +3,16 @@
 # Adds new Gist entries to users pinboard.in account.
 # See https://github.com/dreikanter/gistopin
 
+import os
 from argparse import ArgumentParser
-import configparser
+from configparser import ConfigParser
 import feedparser
+from pprint import pprint, pformat
+import codecs
 
-DEFAULT_CONF = "gistopin.ini"
+DEFAULT_CONF = "./gistopin.ini"
 
 DEFAULT_CONF_SECTION = 'gistopin'
-
-HELP = """For detailed description and latest updates refer to
-project home page at https://github.com/dreikanter/gistopin"""
 
 
 def get_gist_url(username):
@@ -30,27 +30,41 @@ def get_hashtags(text, order=False):
     return sorted(tags) if order else tags
 
 
+def file_exists(file_name):
+    if os.path.isfile(file_name):
+        try:
+            open(file_name)
+        except:
+            return False
+    return True
+
+
 def get_config():
     """Parses command line and returns configuration dictionary"""
     """or termanates the program if --h option specified."""
-    parser = ArgumentParser(description="Gist to pinboard.in importer", epilog=HELP)
+
+    parser = ArgumentParser(description="Gist to pinboard.in importer",
+        epilog="""For detailed description and latest updates refer to """
+            """project home page at https://github.com/dreikanter/gistopin""")
     parser.add_argument("-c", "--conf", default=DEFAULT_CONF, metavar="INI", help="configuration file (default: %s)" % DEFAULT_CONF)
     parser.add_argument("-s", "--section", default=DEFAULT_CONF_SECTION, metavar="SECTION", help="ini section (default: %s)" % DEFAULT_CONF_SECTION)
     args = parser.parse_args()
     print "Using configuration from: %s (section: %s)" % (args.conf, args.section)
 
     try:
-        conf = configparser.ConfigParser()
-        conf.read(args.conf)
-        items = conf.items(args.section)
+        conf = ConfigParser()
+        with open(args.conf, 'rt') as f:
+            conf.readfp(f)
+
         result = dict()
 
-        for item in items:
+        for item in conf.items(args.section):
             result[item[0]] = item[1]
-        if not 'common_tags' in result or not result['common_tags']:
-            result['common_tags'] = list()
+
+        if not 'tags' in result or not result['tags']:
+            result['tags'] = list()
         else:
-            result['common_tags'] = list(set([item.strip() for item in result['common_tags'].split(',')]))
+            result['tags'] = list(set([item.strip() for item in result['tags'].split(',')]))
 
         result['import_private'] = get_bool_param(result, 'import_private')
         result['use_hashtags'] = get_bool_param(result, 'use_hashtags')
@@ -70,12 +84,8 @@ def get_gist_entities(github_user):
     """Returns latest entries from gist feed"""
     url = get_gist_url(github_user)
     print("Retrieving gists from [%s]..." % url)
-    for entry in feedparser.parse(url).entries:
-        yield {
-            'title': entry.title,
-            'link': entry.link,
-            'updated': entry.published_parsed if entry.published_parsed > entry.updated_parsed else entry.updated_parsed,
-            }
+    return [{'title': e.title, 'link': e.link, 'updated': max(e.published_parsed, e.updated_parsed)}
+        for e in feedparser.parse(url).entries]
 
 
 def get_pinboard_entities(user, tags):
@@ -88,68 +98,35 @@ def get_pinboard_entities(user, tags):
     """
 
     url = get_pinboard_url(user, tags)
-    print("Retrieving pinboard bookmarks%s..." %
-        ((" tagged by [%s]" % ", ".join(tags) if len(tags) > 0 else "")))
-    for entry in feedparser.parse(url).entries:
-        yield  {
-            'title': entry['title'],
-            'link': entry['link'],
-            'updated': entry['updated_parsed']
-            }
-
-
-# def get_bookmarks(pinboard_user, pinboard_pwd, tags_list, from_st):
-#     from_dt = datetime.fromtimestamp(mktime(from_st))
-#     print("Retrieving pinboard bookmarks%s since %s..." %
-#         (((" tagged by [%s]" % ", ".join(tags_list) if len(tags_list) > 0 else "")), str(from_dt)))
-
-#     pinboad = pinboard.open(pinboard_user, pinboard_pwd)
-#     posts = pinboad.posts(tags_list)
-#     print(len(posts))
-#     exit()
-#     for item in posts:
-#         print {
-#             'title': item.title,
-#             'link': item.link,
-#             '*': item
-#             }
+    print("Retrieving pinboard bookmarks%s..." % ((" tagged by %s" %
+        ", ".join(map(lambda t: "#" + t, tags)) if len(tags) > 0 else "")))
+    return [{'title': e.title, 'link': e.link, 'updated': e.updated_parsed}
+        for e in feedparser.parse(url).entries]
 
 
 def get_pin_pwd(pwd):
     file_prefix = 'file://'
+
     if not pwd.startswith(file_prefix):
         return pwd
 
-    try:
-        f = file(pwd[len(file_prefix):])
-        result = f.readline().strip()
-        f.close()
-    finally:
-        return result
+    with open(pwd[len(file_prefix):]) as f:
+        return f.readline().strip()
 
 
 def get_new_gists(github_user, pinboard_user, tags):
-    gists = get_gist_entities(github_user)
-    pins = get_pinboard_entities(pinboard_user, tags)
+    gs = dict([(e['link'], e) for e in get_gist_entities(github_user)])
+    ps = dict([(e['link'], e) for e in get_pinboard_entities(pinboard_user, tags)])
+    return filter(gs.has_key, ps.keys())
 
-    if not len(gists) or not len(pins):
-        return gists
-
-    pinsd = {}
-    for pin in pins:
-        pinsd[pin['link']] = pin
-
-    result = []
-    for gist in gists:
-        if not gist['link'] in pinsd or pinsd[gist['link']]['updated'] < gist['updated']:
-            result.append(gist)
-    return result
+    # return filter(lambda g: (not g['link'] in pinsd or pinsd[g['link']] < g['updated']), gists)
 
 
 conf = get_config()
 new_gists = get_new_gists(conf["github_user"], conf['pinboard_user'], conf['tags'])
-if not len(new_gists):
-    print("No new gists found.")
+with codecs.open('debug.txt', 'w', 'utf-8') as f:
+    for item in new_gists:
+        f.write(pformat(item))
 
 #TBD:
 #check if there are new/updated gists (updated > max(updated) from pins)
