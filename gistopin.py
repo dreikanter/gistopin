bb0171a8310b90550c815c1d84084dd82bffe9e8
Alex Musayev <alex.musayev@gmail.com>
2012-06-01 20:04:47 +0400

    Everything is working except the import through pinboard.py module which is apparently obsolete.

--- gistopin.py
+++ gistopin.py
@@ -1,67 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Adds new Gist entries to users pinboard.in account.
-# See https://github.com/dreikanter/gistopin
+# Imports new Gist entries to pinboard.in bookmarks.
+# See https://github.com/dreikanter/gistopin for details.
+# Python 2.7 is required.
 
-import os
+import feedparser
+import pinboard
 from argparse import ArgumentParser
 from configparser import ConfigParser
-import feedparser
 from pprint import pprint, pformat
-import codecs
-from time import mktime
+from time import mktime, sleep
 from datetime import datetime
-import requests
-from requests.auth import HTTPBasicAuth
 from xml.dom.minidom import parse, parseString
-import pinboard
 
 DEFAULT_CONF = "./gistopin.ini"
 DEFAULT_CONF_SECTION = 'gistopin'
-PINBOARD_API_URL = "https://api.pinboard.in/v1/%s"
+DT_FORMAT = "%Y/%m/%d %H:%M:%S"
 DEBUG_MODE = True
-DEBUG_PIN_FEED_URL = "http://localhost/gistopin/pinboard.rss.txt"
-DEBUG_GIST_FEED_URL = "http://localhost/gistopin/gist.atom.txt"
-PINBOARD_MIN_REQ_INTERVAL = 3000
+
+# pinboard._debug = DEBUG_MODE
 
 
 def get_config():
-    """Parses command line and returns configuration dictionary"""
-    """or termanates the program if --h option specified."""
     parser = ArgumentParser(description="Gist to pinboard.in importer",
         epilog="""For detailed description and latest updates refer to """
-            """project home page at https://github.com/dreikanter/gistopin""")
+        """project home page at https://github.com/dreikanter/gistopin""")
+
     parser.add_argument("-c", "--conf", default=DEFAULT_CONF, metavar="INI", help="configuration file (default: %s)" % DEFAULT_CONF)
     parser.add_argument("-s", "--section", default=DEFAULT_CONF_SECTION, metavar="SECTION", help="ini section (default: %s)" % DEFAULT_CONF_SECTION)
     args = parser.parse_args()
+
     print "Using configuration from: %s (section: %s)" % (args.conf, args.section)
 
-    try:
+    def get_params():
         conf = ConfigParser()
         with open(args.conf, 'rt') as f:
             conf.readfp(f)
+        return conf.items(args.section)
 
-        result = dict()
+    def get_tags():
+        return list() if not 'tags' in result or not len(result['tags']) else [
+            item.strip() for item in result['tags'].split(',')]
 
-        for item in conf.items(args.section):
-            result[item[0]] = item[1]
+    def get_bool(param):
+        return True if param in result and result[param].lower() in ['1', 'true'] else False
 
-        if not 'tags' in result or not result['tags']:
-            result['tags'] = list()
-        else:
-            result['tags'] = list(set([item.strip() for item in result['tags'].split(',')]))
-
-        def get_bool(conf, param):
-            return True if param in conf and conf[param].lower() in ['1', 'true'] else False
-
-        result['import_private'] = get_bool(result, 'import_private')
-        result['use_hashtags'] = get_bool(result, 'use_hashtags')
-
+    def get_pin_pwd():
         file_prefix = 'file://'
-        if result['pinboard_pwd'].startswith(file_prefix):
-            with open(result['pinboard_pwd'][len(file_prefix):]) as f:
-                result['pinboard_pwd'] = f.readline().strip()
+        pwd = result['pinboard_pwd']
+        if pwd.startswith(file_prefix):
+            with open(pwd[len(file_prefix):]) as f:
+                return f.readline().strip()
+        return pwd
+
+    try:
+        result = {item[0]: item[1] for item in get_params()}
+
+        result['tags'] = get_tags()
+        result['import_private'] = get_bool('import_private')
+        result['use_hashtags'] = get_bool('use_hashtags')
+        result['pinboard_pwd'] = get_pin_pwd()
 
         return result
 
@@ -69,42 +68,25 @@ def get_config():
         print "Error reading configuration:", e
         exit()
 
-
 conf = get_config()
 
 
 def get_gist_url():
-    return DEBUG_GIST_FEED_URL if DEBUG_MODE else (
-        "http://gist.github.com/%s.atom" % conf['github_user'])
-
-
-# def get_pinboard_url(username, tags):
-#     return DEBUG_PIN_FEED_URL if DEBUG_MODE else "http://feeds.pinboard.in/rss/u:%s/%s" % (username, '/'.join(["t:" + item for item in tags]))
-
-
-# def get_pinapi_url(method):
-#     return PINBOARD_API_URL % method
+    return "http://gist.github.com/%s.atom" % conf['github_user']
 
 
 def extract_hashtags(text):
     """Extracts distinct hashtag collection from string"""
-    tags = set([item.strip("#.,-\"\'&*^!") for item in text.split()
-        if (item.startswith("#") and len(item) < 256)])
-    return tags
+    return list(set([item.strip("#.,-\"\'&*^!") for item in text.split()
+        if (item.startswith("#") and len(item) < 256)]))
 
 
-def struct_time_str(st, format="%Y/%m/%d %H:%M:%S"):
-    return datetime.fromtimestamp(mktime(st)).strftime(format)
+def st2dt(st):
+    return datetime.fromtimestamp(mktime(st))
 
 
-# def file_exists(file_name):
-#     if os.path.isfile(file_name):
-#         try:
-#             open(file_name)
-#             return True
-#         except:
-#             pass
-#     return False
+def struct_time_str(st, format=DT_FORMAT):
+    return st2dt(st).strftime(DT_FORMAT)
 
 
 def get_gist_entities():
@@ -116,34 +98,12 @@ def get_gist_entities():
             for e in feedparser.parse(url).entries]
 
 
-# def get_pinboard_entities(user, tags):
-#     """Returns latest bookmarks from pinboard.in feed
-#     Args:
-#         user: Pinboard user name.
-#         tags: Tags list.
-#     Returns:
-#         List of dicts containing 'tile', 'link' and 'updated' items in each item.conf['github_user']#     """
-
-    # url = get_pinboard_url(user, tags)
-    # print("Retrieving pinboard bookmarks%s..." % ((" tagged by %s" %
-#         ", ".join(map(lambda t: "#" + t, tags)) if len(tags) > 0 else "")))
-#     return [{'title': e.title, 'link': e.link, 'updated': e.updated_parsed}
-#         for e in feedparser.parse(url).entries]
-
-
-# TODO: Limit date/time with the oldes gist utime
-def get_pinboard_entries(pinboard):
+def get_pinboard_entries(pinboard, fromdt):
     """Returns url:utime dictionary for specified pinboard tags"""
-    print("Retrieving pinboard posts tagged with [%s]..." % ", ".join(conf['tags']))
-    return {item['href']: struct_time_str(item['time_parsed']) for item in pinboard.posts(tag=" ".join(conf['tags']))}
-
-
-# def get_new_gists(github_user, pinboard_user, tags):
-#     gists = get_gist_entities(github_user)
-#     # Note: Set comprehensions (added in python 2.7) causes SublimeLinter to display syntax error
-#     pins = {e['link']: e['updated'] for e in get_pinboard_entities(pinboard_user, tags)}
-#     print("Got %d existing bookmarks and %d gists" % (len(pins), len(gists)))
-#     return filter(lambda g: not g['link'] in pins, gists), filter(lambda g: g['link'] in pins and pins[g['link']] < g['updated'], gists)
+    print("Retrieving pinboard bookmarks from %s tagged with [%s]..." %
+        (fromdt.strftime(DT_FORMAT), ", ".join(conf['tags'])))
+    posts = pinboard.posts(tag=" ".join(conf['tags']), fromdt=fromdt)
+    return {item['href']: item['time_parsed'] for item in posts}
 
 
 def get_new_gists(gists, pins):
@@ -155,69 +115,24 @@ def get_new_gists(gists, pins):
 def post_gists():
     pnb = pinboard.PinboardAccount(conf['pinboard_user'], conf['pinboard_pwd'])
     gists = get_gist_entities()
-    pins = get_pinboard_entries(pnb)
-    print("Got %d existing bookmarks and %d gists" % (len(pins), len(gists)))
-    gists = get_new_gists(gists, pins)
-    print("Importing %d gists to pinboard..." % len(gists))
+    pins = get_pinboard_entries(pnb, st2dt(min([g['utime'] for g in gists])))
+    print("Got %d existing bookmarks and %d gists." % (len(pins), len(gists)))
+    new_gists, updated_gists = get_new_gists(gists, pins)
     errors_cnt = 0
-    for gist in gists:
-        # gist_tags=conf['tags']+extract_hashtags(gist['description'])
-        # pnb.add(gists["url"], gists["description"], tags=gist_tags, replace="yes")
-        pprint(gists)
+
+    def post(gists, gists_type):
+        if gists:
+            print("Importing %d %s gists to pinboard..." % (len(gists), gists_type))
+            for g in gists:
+                print(" + %s..." % g['href'])
+                # TODO: Add replace parameter to pinboard module
+                # TODO: Add replace="yes" parameter to add() call
+                tags = set(extract_hashtags(g['description']) + conf['tags'])
+                pnb.add(g["href"], g["description"], tags=tags)
+
+    post(new_gists, "new")
+    post(updated_gists, "updated")
 
     print("Import succeeded." if not errors_cnt else "Import completed with %d errors." % errors_cnt)
 
-# def yesno(value):
-#     return "yes" if value else "no"
-
-
-# def pin_request(user, pwd, method, params):
-#     """Performs GET request to Pinboard API"""
-
-#     r = requests.get(get_pinapi_url(method), auth=HTTPBasicAuth(user, pwd), params=params)
-
-#     if DEBUG_MODE:
-#         print "Performed GET request:", r.url
-#         print "Status:", r.status_code
-#         print("\n\n")
-#         pprint(r)
-
-#     return r
-
-
-# def add_bookmark(url, description, tags, shared, user, pwd):
-#     """Adds new (or ovewrite existing) bookmark to pinboard.in"""
-
-#     r = pin_request(user, pwd, "posts/add", params={
-#         "url": url.strip(),
-#         "description": description.strip(),
-#         "tags": " ".join(filter(bool, [t.strip() for t in tags])),
-#         "replace": yesno(True),
-#         "shared": yesno(shared),
-#         "toread": yesno(False),
-#     })
-
-#     return True
-
-
 post_gists()
-
-#   u'description': u'PHP Filters',
-#   u'extended': u'',
-#   u'hash': u'dcebb38dcc214853b9d91b531365eac8',
-#   u'href': u'http://www.owasp.org/software/labs/phpfilters.html',
-#   u'tags': [u'php', u'webdev', u'snippets'],
-#   u'time': u'2005-07-06T06:52:30Z',
-#   u'time_parsed': time.struct_time(tm_year=2005, tm_mon=7, tm_mday=6, tm_hour=6, tm_min=52, tm_sec=30, tm_wday=2, tm_yday=187, tm_isdst=-1)}]
-
-# add_bookmark("http://microsoft.com", "Microsoft Corp.", ['companies', 'microsoft'],
-#     False, conf['pinboard_user'], get_pin_pwd(conf['pinboard_pwd']))
-# exit()
-
-# new_gists, updated_gists = get_new_gists(conf["github_user"], conf['pinboard_user'], conf['tags'])
-
-# print("\n\nnew_gists")
-# pprint(new_gists)
-
-# print("\n\nupdated_gists")
-# pprint(updated_gists)
