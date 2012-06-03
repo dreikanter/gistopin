19fea8f976edf06a8f3079e4dbd1d41d8184dd86
Alex Musayev <alex.musayev@gmail.com>
2012-06-03 18:01:36 +0400

    Some deployment-related minor updates.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
-# TBD
-# http://stackoverflow.com/questions/4740473/setup-py-examples
-# http://packages.python.org/an_example_pypi_project/setuptools.html
-
 from distutils.core import setup
 
 setup(name='gistopin',
-    version='0.1',
-    description='Python script to bookmark new Gist entries with pinboard.in service',
+    version='0.0.1',
     author='Alex Musayev',
-    author_email='alex.usayev@gmail.com',
+    author_email='alex.musayev@gmail.com',
     url='https://github.com/dreikanter/gistopin',
-    scripts=['gistopin'])
-
-#['feedparser', 'configparser', 'python-pinboard']
+    description='Python script to bookmark new Gist entries with pinboard.in service',
+    long_description=open('README.md').read(),
+    scripts=['gistopin.py'],
+    license="BSD",
+    requires=['feedparser', 'configparser'],
+    py_modules=['pinboard'],
+    data_files=[('config', ['gistopin.ini'])],)
